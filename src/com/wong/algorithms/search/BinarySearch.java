a679f60ebc338ec895724ea319ce1428078c823a
Edwardwong0826 <58102922+Edwardwong0826@users.noreply.github.com>
2022-05-19 18:13:37 +0800

    Add files via upload

--- src/com/wong/algorithms/search/BinarySearch.java
+++ src/com/wong/algorithms/search/BinarySearch.java
@@ -1,14 +1,42 @@
 package com.wong.algorithms.search;
 
+import java.util.HashMap;
+
 public class BinarySearch {
 
 
     public static void main(String[] args)
     {
-        int arr[] = {1,8,10,89,1000,1234};
-        int resultIndex = binarySearchRecursion(arr,0,arr.length-1,12);
-        System.out.println(resultIndex);
+        int[] arr = {1,8,10,89,1000,1234};
+        int[] arr2 = {1,3,5,6};
+        //int resultIndex = binarySearchRecursion(arr,0,arr.length-1,12);
+        int index = binarySearchIterative(arr2,2);
 
+        //System.out.println(resultIndex);
+         System.out.println(index);
+
+    }
+
+    public int singleNumber(int[] nums) {
+
+        HashMap<Integer,Integer> hash = new HashMap<>();
+
+        for(int num: nums){
+            if(hash.containsKey(num))
+            {
+                int count = hash.get(num);
+                hash.put(num,++count);
+            }
+            else
+                hash.put(num,1);
+        }
+
+        for(int num : hash.keySet()){
+            if(hash.get(num) == 1)
+                return num;
+        }
+
+        return -1;
     }
 
     public static int binarySearchRecursion(int[] arr, int left, int right, int findVal)
@@ -20,13 +48,14 @@ public class BinarySearch {
             return -1;
         }
 
-        if(findVal > midVal)
-        {
-            return binarySearchRecursion(arr,mid+1,right,findVal);
-        }
-        else if(findVal < midVal)
+        if(findVal < midVal)
         {
             return binarySearchRecursion(arr,left,mid-1,findVal);
+        }
+        else if(findVal > midVal)
+        {
+            return binarySearchRecursion(arr,mid+1,right,findVal);
+
         }
         else
         {
@@ -34,27 +63,26 @@ public class BinarySearch {
         }
     }
 
-    public static int binarySearchIterative(int[] arr, int target)
+    // this iterative can search index of value and return index to insert when target is not present
+    public static int binarySearchIterative(int[]arr, int target)
     {
+        int left = 0;
+        int right = arr.length - 1;
 
-            if (arr == null || arr.length == 0) return -1;
-            int left = 0;
-            int right = arr.length - 1;
+        while(left<=right)
+        {
+            int mid = left + (right - left) / 2;
 
-            while(left<=right)
-            {
-                int mid = left + (right - left) / 2;
+            if(arr[mid] == target)
+                return mid;
 
-                if(arr[mid] == target)
-                    return mid;
+            if(arr[mid] > target)
+                right = mid -1;
+            else
+                left = mid + 1;
+        }
 
-                if(arr[mid] > target)
-                    right = mid -1;
-                else
-                    right = mid + 1;
-            }
-
-            return -1;
+        return left;
     }
 
     public static int binarySearchUseInsertValue(int[]arr, int left, int right, int findVal)
