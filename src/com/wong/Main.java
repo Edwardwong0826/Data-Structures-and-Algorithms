011319273c37de030b9652d4b5adc89f2e93739a
Edwardwong0826 <edwardwong_94@outlook.com>
2023-01-05 18:12:05 +0800

    Update leetcode question answer

--- src/com/wong/Main.java
+++ src/com/wong/Main.java
@@ -290,6 +290,29 @@ public class Main {
 
     // Array category problem
 
+    // Question 1 Two Sum
+    public static int[] twoSum1(int[] nums, int target) {
+
+        int[] result = new int[2];
+
+        for(int i = 0; i < nums.length; i++){
+
+            for(int j=i+1; j < nums.length; j++){
+
+                if(nums[i] + nums[j] == target){
+                    result[0] = i;
+                    result[1] = j;
+
+                }
+
+            }
+
+        }
+
+
+        return result;
+    }
+
     // Question 26 Remove Duplicates from Sorted Array - String
     public static int removeDuplicates(int[] nums) {
 
